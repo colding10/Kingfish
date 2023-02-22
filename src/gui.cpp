85590af7316bc6e57f8b89fbde6a1c2c6faba33f
Colin D <105526525+colding10@users.noreply.github.com>
2023-02-22 12:10:28 -0500

    rename bmp folder to icons

--- src/gui.cpp
+++ src/gui.cpp
@@ -105,7 +105,7 @@ void GUI::drawChessboard(SDL_Renderer* renderer, Board* board, TTF_Font* font) {
             p = board->getPieceAt(Location(i, j));
 
             if (p != 0) {
-                std::string filepath = "../assets/bmp/" + Pieces::getPieceFilename(p);
+                std::string filepath = "../assets/icons/" + Pieces::getPieceFilename(p);
 
                 SDL_Surface* image = SDL_LoadBMP(filepath.c_str());
                 if (image == NULL) {
