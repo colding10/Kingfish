6445a30fb04b634345ff967e8416c6604bd7f78d
Colin D <colinding10@gmail.com>
2023-04-11 11:30:57 -0400

    Add killer move pruning

--- src/searcher.cpp
+++ src/searcher.cpp
@@ -61,18 +61,18 @@ int Searcher::bound(Position &pos, int gamma, int depth, bool can_null = true) {
         }
 
         int val_lower = depth == 0 ? QS : -MATE_LOWER;
-        // if (tp_move.count(pos.hash())) {
-        //     Move killer = this->tp_move.at(pos.hash());
-        //     if (depth > 2) {
-        //         this->bound(pos, gamma, depth - 3, false);
-        //         killer = this->tp_move.at(pos.hash());
-        //     }
-        //     if (pos.value(killer) >= val_lower) {
-        //         Position moved_board = pos.move(killer);
-        //         all_moves.push_back(
-        //             {killer, -this->bound(moved_board, 1 - gamma, depth - 1)});
-        //     }
-        // }
+        if (tp_move.count(pos.hash())) {
+            Move killer = this->tp_move.at(pos.hash());
+            if (depth > 2) {
+                this->bound(pos, gamma, depth - 3, false);
+                killer = this->tp_move.at(pos.hash());
+            }
+            if (pos.value(killer) >= val_lower) {
+                Position moved_board = pos.move(killer);
+                all_moves.push_back(
+                    {killer, -this->bound(moved_board, 1 - gamma, depth - 1)});
+            }
+        }
         std::vector<std::pair<int, Move>> rest_moves;
         for (auto m : pos.genMoves()) {
             rest_moves.push_back({pos.value(m), m});
