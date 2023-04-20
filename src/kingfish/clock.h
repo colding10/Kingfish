b640e297bc1346d76bcf39a31e14599f3dc3b60c
Colin D <colinding10@gmail.com>
2023-04-19 22:41:36 -0400

    Update

---
+++ src/kingfish/clock.h
@@ -0,0 +1,17 @@
+#ifndef KINGFISH_CLOCK_H_INCLUDED
+#define KINGFISH_CLOCK_H_INCLUDED
+
+#include <chrono>
+
+#include "types.h"
+
+using Clock     = std::chrono::high_resolution_clock;
+using TimePoint = std::chrono::time_point<Clock, Clock::duration>;
+
+inline i64 deltaMs(TimePoint later, TimePoint earlier) {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(later -
+                                                                 earlier)
+        .count();
+}
+
+#endif // KINGFISH_CLOCK_H_INCLUDED
